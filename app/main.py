d7515dab45c3d8b5cb1d39864a69f7e73a35dcec
DongHoon Lee <dhl1924@naver.com>
2024-12-14 13:30:10 +0900

    fix: 잘못된 주석처리 수정

--- app/main.py
+++ app/main.py
@@ -56,29 +56,29 @@ async def create_meet(file_url: str, username: str = ''):
 
 
 # unique id로 get 요청 - 해당 url에 속하는 사람들의 교집합 return
-@app.post("/meet")
-async def add_timetable(id: str = '', file: UploadFile = File(...), username: str = ''):
-    # 존재하는 url인지부터 확인
-    meets = await databaseModule.filter_meet(id)
-
-    if not meets:
-        return {"error": "해당하는 url이 존재하지 않습니다."}
-    else:
-        # 유저의 시간표 read
-        image = convertImgFormat.load_image_into_numpy_array(await file.read())
-        timetable = exportImg.export_img(image)
-
-        # 유저의 시간표 받은 id에 해당하는 url로 db에 저장
-        databaseModule.savedb(username, json.dumps(timetable, ensure_ascii=False), id)
-
-        # 겹치는 meet 가져오기
-        meets = await databaseModule.filter_meet(id)
-
-        # 겹치는 시간대 전부 표시하는 알고리즘
-        res = management.filter_table(meets)
-
-        # return res
-        return {"meets": res}
+# @app.post("/meet")
+# async def add_timetable(id: str = '', file: UploadFile = File(...), username: str = ''):
+#     # 존재하는 url인지부터 확인
+#     meets = await databaseModule.filter_meet(id)
+#
+#     if not meets:
+#         return {"error": "해당하는 url이 존재하지 않습니다."}
+#     else:
+#         # 유저의 시간표 read
+#         image = convertImgFormat.load_image_into_numpy_array(await file.read())
+#         timetable = exportImg.export_img(image)
+#
+#         # 유저의 시간표 받은 id에 해당하는 url로 db에 저장
+#         databaseModule.savedb(username, json.dumps(timetable, ensure_ascii=False), id)
+#
+#         # 겹치는 meet 가져오기
+#         meets = await databaseModule.filter_meet(id)
+#
+#         # 겹치는 시간대 전부 표시하는 알고리즘
+#         res = management.filter_table(meets)
+#
+#         # return res
+#         return {"meets": res}
 
 
 # unique id로 get 요청 - 해당 url에 속하는 사람들의 교집합 return
@@ -103,23 +103,23 @@ async def filter_timetable(entrance_code: str = ''):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
-# @app.post("/entrance-codes")
-# async def save_code(entrance_code: str, user_id: int):
-#     username = str(user_id)
-#
-#     # username으로 매칭되는 Notion 페이지를 검색
-#     page_id = databaseModule.find_page_by_username(username)
-#
-#     if not page_id:
-#         raise HTTPException(status_code=404, detail="Page not found for the user")
-#
-#     # Notion 페이지에 entranceCode 저장
-#     success = databaseModule.update_page_with_code(page_id, entrance_code)
-#
-#     if success:
-#         return {"entrance_code": entrance_code, "user_id": username}
-#     else:
-#         raise HTTPException(status_code=500, detail="Failed to save the entrance code.")
+@app.post("/entrance-codes")
+async def save_code(entrance_code: str, user_id: int):
+    username = str(user_id)
+
+    # username으로 매칭되는 Notion 페이지를 검색
+    page_id = databaseModule.find_page_by_username(username)
+
+    if not page_id:
+        raise HTTPException(status_code=404, detail="Page not found for the user")
+
+    # Notion 페이지에 entranceCode 저장
+    success = databaseModule.update_page_with_code(page_id, entrance_code)
+
+    if success:
+        return {"entrance_code": entrance_code, "user_id": username}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to save the entrance code.")
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True)
